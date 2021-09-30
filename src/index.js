3251fc8ed3078054335cc8311f350171100b6f87
Andrii Solohub <andrei.sologub12@gmail.com>
2021-09-30 21:17:40 +0300

    renamed file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 const { runCommand } = require('./commands');
 const { CARDINAL_POINTS, DEFAULT_COORDINATE, INITIAL_STEP } = require('./constants/constants');
 const { getFormattedCommand } = require('./helpers/get-formatted-command');
-const { isCommandTypeOfPlace } = require('./helpers/is-command-type');
+const { isCommandTypeOfPlace } = require('./helpers/is-command-type-place');
 const { isCommandsValid } = require('./helpers/is-commands-valid');
 const { isFirstCommandFormatValid } = require('./helpers/is-first-command-format-valid');
 
